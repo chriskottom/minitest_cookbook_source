6f03fcd7d6213445ef9b0c587ac31190d7345386
Chris Kottom <chris@chriskottom.com>
2015-03-21 17:27:44 +0100

    changing behaves_like class method definition

--- sharing_code/test/test_helper.rb
+++ sharing_code/test/test_helper.rb
@@ -5,10 +5,8 @@ require 'support/fizzing_buzzing_tests'
 
 module Minitest
   class Test
-    class << self
-      def behaves_like(_module)
-        include _module
-      end
+    def self.behaves_like(_module)
+      include _module
     end
   end
 end
