8679a70f6fb96df8396c6a9fe0e9475b1dc74942
Chris Kottom <chris@chriskottom.com>
2015-04-19 13:15:53 +0200

    use anonymous classes when possible to avoid polluting global namespace

--- greetable/test/greetable_test.rb
+++ greetable/test/greetable_test.rb
@@ -1,31 +1,34 @@
 require 'test_helper'
 
 class GreetableTest < Minitest::Test
-  ThingWithName = Struct.new(:name) do
-    include Greetable
-  end
-
   def test_generic_greeting_delivered
-    @greetable = Object.new
-    @greetable.extend(Greetable)
+    greetable = Object.new
+    greetable.extend(Greetable)
 
     assert_output 'Ohai!' do
-      @greetable.greet
+      greetable.greet
     end
   end
 
-  def test_personalized_greeting_delivered
-    # @greetable = ThingWithName.new('Matz')
-    @greetable = Object.new
-    @greetable.extend(Greetable)
-    class << @greetable
-      def name
-        'Matz'
-      end
-    end
-
+  def test_personalized_greeting_delivered_using_struct
     assert_output('Hey, Matz.') do
-      @greetable.greet
+      greetable = Struct.new(:name) do
+        include Greetable
+      end.new("Matz")
+      greetable.greet
+    end
+  end
+
+  def test_personalized_greeting_delivered_using_object
+    assert_output('Hey, Matz.') do
+      greetable = Object.new
+      greetable.extend(Greetable)
+      class << greetable
+        def name
+          'Matz'
+        end
+      end
+      greetable.greet
     end
   end
 end
