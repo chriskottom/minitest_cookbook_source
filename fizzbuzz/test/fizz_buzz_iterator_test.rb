3a95b9920b784e4bdce244eb7e41cbb2a668bd79
Chris Kottom <chris@chriskottom.com>
2015-11-02 18:22:56 +0100

    update FizzBuzz for Minitest 5.8, #expect spec syntax

--- fizzbuzz/test/fizz_buzz_iterator_test.rb
+++ fizzbuzz/test/fizz_buzz_iterator_test.rb
@@ -5,24 +5,24 @@ describe FizzBuzzIterator do
   subject { FizzBuzzIterator.new }
 
   it 'outputs sequential FizzBuzz values starting from 1' do
-    subject.next.must_equal '1'
-    subject.next.must_equal '2'
-    subject.next.must_equal 'Fizz'
-    subject.next.must_equal '4'
-    subject.next.must_equal 'Buzz'
-    subject.next.must_equal 'Fizz'
+    expect(subject.next).must_equal '1'
+    expect(subject.next).must_equal '2'
+    expect(subject.next).must_equal 'Fizz'
+    expect(subject.next).must_equal '4'
+    expect(subject.next).must_equal 'Buzz'
+    expect(subject.next).must_equal 'Fizz'
   end
 
   describe 'when initialized with a starting value' do
     subject { FizzBuzzIterator.new(10) }
 
     it 'outputs sequential FizzBuzz values starting from the starting value' do
-      subject.next.must_equal 'Buzz'
-      subject.next.must_equal '11'
-      subject.next.must_equal 'Fizz'
-      subject.next.must_equal '13'
-      subject.next.must_equal '14'
-      subject.next.must_equal 'FizzBuzz'
+      expect(subject.next).must_equal 'Buzz'
+      expect(subject.next).must_equal '11'
+      expect(subject.next).must_equal 'Fizz'
+      expect(subject.next).must_equal '13'
+      expect(subject.next).must_equal '14'
+      expect(subject.next).must_equal 'FizzBuzz'
     end
   end
 end
