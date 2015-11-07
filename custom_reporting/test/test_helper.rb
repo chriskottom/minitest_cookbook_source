b7ed64406301669057f8894730fe76b57f51561f
Chris Kottom <chris@chriskottom.com>
2015-11-07 18:04:59 +0100

    update gem versions, test helper for 'Customizing Test Reports' recipe

--- custom_reporting/test/test_helper.rb
+++ custom_reporting/test/test_helper.rb
@@ -1,6 +1,8 @@
 require 'minitest/autorun'
 
 # require 'minitest/reporters'
+# Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new]
+# Minitest::Reporters.use! [Minitest::Reporters::ProgressReporter.new]
 # Minitest::Reporters.use! [Minitest::Reporters::SpecReporter.new]
 
 require 'support/reporters'
