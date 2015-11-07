017f07a875ec12bc0de188179183fea93280adb1
Chris Kottom <chris@chriskottom.com>
2015-11-07 22:11:29 +0100

    update Minitest and minitest-rails versions, spec syntax (expect) for 'Writing Custom Assertions and Expectations' recipe

--- camp_base/test/controllers/sessions_controller_spec_test.rb
+++ camp_base/test/controllers/sessions_controller_spec_test.rb
@@ -8,12 +8,12 @@ describe "SessionsController", "spec-style" do
   describe "POST :create" do
     it "logs the the user in with good credentials" do
       post :create, login: user.login, password: "secret"
-      user.must_be_logged_in
+      expect(user).must_be_logged_in
     end
 
     it "does not log the user in with bad credentials" do
       post :create, login: user.login, password: "invalid"
-      must_not_be_logged_in
+      expect(response).must_not_be_logged_in
     end
   end
 
@@ -21,7 +21,7 @@ describe "SessionsController", "spec-style" do
     it "logout ends the user's session" do
       log_in_user(user)
       delete :destroy
-      must_not_be_logged_in
+      expect(response).must_not_be_logged_in
     end
   end
 
