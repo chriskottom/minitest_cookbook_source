8c9d71d6837bd42b8f511b38db680bf97fcc35ec
Chris Kottom <chris@chriskottom.com>
2015-12-09 05:16:59 +0100

    update ConfirmationEmailSender to use #deliver_later

--- minishop/app/models/order/confirmation_email_sender.rb
+++ minishop/app/models/order/confirmation_email_sender.rb
@@ -1,7 +1,7 @@
 class Order < ActiveRecord::Base
   class ConfirmationEmailSender
     def after_commit(order)
-      Notifier.order_received(order).deliver_now
+      Notifier.order_received(order).deliver_later
     end
   end
 end
--- minishop/test/models/order/confirmation_email_sender_test.rb
+++ minishop/test/models/order/confirmation_email_sender_test.rb
@@ -2,16 +2,29 @@ require "test_helper"
 
 describe Order::ConfirmationEmailSender, :model do
   describe "#after_commit" do
-    it "sends a confirmation email to the user who placed the order" do
-      order = orders(:barebones)
-      sender = Order::ConfirmationEmailSender.new
+    include ActionMailer::TestHelper
 
-      assert_difference "ActionMailer::Base.deliveries.count" do
-        sender.after_commit(order)
+    before do
+      ActionMailer::Base.deliveries.clear
+      @order = orders(:barebones)
+      @sender = Order::ConfirmationEmailSender.new
+    end
+
+    it "queues a confirmation email" do
+      assert_enqueued_emails 1 do
+        @sender.after_commit(@order)
       end
+    end
 
-      mail = ActionMailer::Base.deliveries.last
-      expect(mail[:subject].value).must_equal "MiniShop Order Confirmation"
+    it "sends the confirmation to the sender who placed the order" do
+      assert_performed_jobs(1, only: ActionMailer::DeliveryJob) do
+        @sender.after_commit(@order)
+      end
+      assert_emails 1
+
+      mail = ActionMailer::Base.deliveries.first
+      expect(mail.subject).must_equal "MiniShop Order Confirmation"
+      expect(mail.to).must_equal [@order.email]
     end
   end
 end
