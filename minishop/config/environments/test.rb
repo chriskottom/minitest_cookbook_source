f63ea581d9fe8e8611c291c6d44f4bc221a62dce
chriskottom <chris@chriskottom.com>
2016-11-22 13:35:58 +0100

    always use the :test QueueAdapter for Active Job in the test environment

--- minishop/config/environments/test.rb
+++ minishop/config/environments/test.rb
@@ -42,4 +42,7 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Always use the testing queue adapter for Active Job.
+  config.active_job.queue_adapter = :test
 end
